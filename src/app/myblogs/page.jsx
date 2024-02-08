b6b66e302f565b8cff92d9b05d72ada6cedc2deb
YASH GUPTA <yash2002.gupta@gmail.com>
2024-02-08 23:15:15 +0530

    change

--- src/app/myblogs/page.jsx
+++ src/app/myblogs/page.jsx
@@ -1,8 +1,10 @@
 import MyBlogs from '@/components/MyBlogs'
 import React from 'react'
 
-export default function myblogs() {
+export default function page() {
   return (
-    <MyBlogs />
+    <div>
+      <MyBlogs/>
+    </div>
   )
 }
