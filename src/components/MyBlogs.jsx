07fe74a5ec21734926d5043d40cbb1385d2ea610
YASH GUPTA <yash2002.gupta@gmail.com>
2024-02-08 23:11:28 +0530

    update myBlogs

---
+++ src/components/MyBlogs.jsx
@@ -0,0 +1,26 @@
+'use client'
+import Feed from '@/components/Feed'
+import Header from '@/components/Header'
+import UploadModal from '@/components/UploadModal'
+import { useSession } from 'next-auth/react'
+import {useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
+
+export default function MyBlogs() {
+    const {data:session} = useSession()
+    const router = useRouter()
+    if (!session) {
+        router.push('/')
+    }
+    else{
+        return (
+            <div className="min-h-screen" style={{ backgroundImage: 'url("https://static.vecteezy.com/system/resources/previews/026/184/692/non_2x/seamless-pattern-with-autumn-fall-leaves-in-beige-red-brown-green-and-yellow-perfect-for-wallpaper-wrapping-paper-web-sites-background-social-media-blog-and-greeting-cards-vector.jpg")'   }}>
+            <div className='backdrop-blur-[2px] min-h-screen '>
+            <Header />
+            <Feed />
+            <UploadModal />
+            </div>
+          </div>
+        )
+    }
+}
